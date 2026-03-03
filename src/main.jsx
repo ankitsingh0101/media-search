313f2782acaf46415a7b692d4e90ebd110b8c006
Ankit Singh <ankitali45161@gmail.com>
2026-03-03 13:38:27 +0530

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,16 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
+        <Analytics />   {/* Add this here */}
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
